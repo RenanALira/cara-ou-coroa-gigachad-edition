06fb061b5c1af893dc5b9d75b1432ba5e026e663
Renan Alves de Lira <renanlira.dev@gmail.com>
2025-10-02 17:55:36 -0300

    Travei a orientação para portraitUp

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:cara_ou_coroa_gigachad_edition/shared/custom_themes/custom_theme
 import 'package:cara_ou_coroa_gigachad_edition/features/coin/viewmodel/coin_viewmodel.dart';
 import 'package:cara_ou_coroa_gigachad_edition/features/coin/view/coin_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(
     MultiProvider(
       providers: [ChangeNotifierProvider<CoinViewModel>(create: (_) => CoinViewModel())],
@@ -20,10 +25,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Cara ou Coroa: Gigachad Edition",
-      home: const CoinView(),
       themeMode: ThemeMode.system,
       theme: CustomTheme.lightTheme,
       darkTheme: CustomTheme.darkTheme,
+      home: const CoinView(),
     );
   }
 }
